0c13b7c38fd33f30552acde2dcfa78e6128c70bd
chrisyue <blizzchris@gmail.com>
2020-07-17 00:30:49 +0800

    fixup

--- pipe.js
+++ pipe.js
@@ -2,20 +2,34 @@ const obfuscator = require('./obfuscator.js');
 const chunk = require('./chunk.js');
 
 module.exports = (to, shouldObfuscate, key) => {
+    let i = 0;
     if (shouldObfuscate) {
         return message => {
+            if (i > 5) {
+                to.write(message);
+
+                return;
+            }
+
             const obfuscated = obfuscator.obfuscate(message, key);
             const joined = chunk.join([obfuscated]);
-
             to.write(joined);
+            ++i;
         };
     }
 
+    let j = 0;
     return message => {
         chunk.split(message).forEach(element => {
-            const cleared = obfuscator.recover(element, key);
+            if (j > 5) {
+                to.write(message);
 
+                return;
+            }
+
+            const cleared = obfuscator.recover(element, key);
             to.write(cleared);
+            ++j;
         });
     };
 };
