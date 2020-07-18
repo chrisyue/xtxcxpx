e1220eaf0483446b223ef56d199cd706ce0bc02e
chrisyue <blizzchris@gmail.com>
2020-07-18 12:49:32 +0800

    fake http header

--- obfuscator.js
+++ obfuscator.js
@@ -3,14 +3,17 @@ const crypto = require('crypto');
 const method = 'aes-128-cbc';
 const ivLength = 16;
 
+const header = 'GET /a HTTP/2\r\n\r\n';
+
 const obfuscate = (data, key) => {
     const iv = crypto.randomBytes(ivLength);
     const cipher = crypto.createCipheriv(method, Buffer.from(key), iv);
 
-    return  Buffer.concat([iv, cipher.update(data), cipher.final()]);
+    return Buffer.concat([Buffer.from(header), iv, cipher.update(data), cipher.final()]);
 };
 
 const recover = (data, key) => {
+    data = data.subarray(header.length);
     const iv = data.subarray(0, ivLength);
     const decipher = crypto.createDecipheriv(method, Buffer.from(key), iv);
 
--- pipe.js
+++ pipe.js
@@ -1,6 +1,5 @@
 const obfuscator = require('./obfuscator.js');
 const chunk = require('./chunk.js');
-// const fs = require('fs');
 
 module.exports = (to, shouldObfuscate, key) => {
     if (shouldObfuscate) {
@@ -8,25 +7,14 @@ module.exports = (to, shouldObfuscate, key) => {
             const obfuscated = obfuscator.obfuscate(message, key);
             const joined = chunk.join([obfuscated]);
 
-            // fs.writeFileSync('to.log', joined.toString('hex') + '\n', { flag: 'a' });
-
             to.write(joined);
         };
     }
 
     return message => {
-        // fs.writeFileSync('from.log', message.toString('hex') + '\n', { flag: 'a' });
-
         chunk.split(message).forEach(element => {
-            // try {
             const cleared = obfuscator.recover(element, key);
             to.write(cleared);
-            // } catch (ex) {
-            //     fs.writeFileSync('err.log', message.toString('hex') + '\n', { flag: 'a' });
-            //     fs.writeFileSync('err.log', element.toString('hex') + '\n', { flag: 'a' });
-            //
-            //     throw ex;
-            // }
         });
     };
 };
